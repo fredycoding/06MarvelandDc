f6a21e480f9af331c5041e8307073ad03850fee8
ASUSTANUS <xbox360colombia@gmail.com>
2022-08-30 20:14:16 -0500

    transiciones

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,9 @@ export const Home = () => {
     <div className='text-center'>
    
     <h1 className=' text-5xl mt-12'>SUPER HEROES DE DC Y MARVEL</h1>
-    <div className='grid-cols-2 inline-grid text-center gap-6 align-middle mt-9'>
-     <a href="/marvel"><img src={MarvelLogo} className="h-44 hover:h-60" alt="" /></a>
-     <a href="/dc"><img src={DcLogo} className="h-44 hover:h-60 hover:translate-x-5" alt="" /></a>
+    <div className='grid-cols-2 inline-grid text-center gap-6 align-middle mt-20'>
+     <a href="/marvel"><img src={MarvelLogo} className="h-44 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-150 hover:bg-indigo-500 duration-300 rounded-md" alt="" /></a>
+     <a href="/dc"><img src={DcLogo} className="h-44 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-150 hover:bg-indigo-500 duration-300 rounded-md" alt="" /></a>
     </div>
     </div>
     
