353524fa891d7ec6619b73ab5e9285560730aafb
ASUSTANUS <xbox360colombia@gmail.com>
2022-09-01 17:38:01 -0500

    Agregando biblioteca de animaciones de https://animate.style/

--- src/components/HeroeDetalle.jsx
+++ src/components/HeroeDetalle.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from 'axios'
+import 'animate.css'
 
 
 export const HeroeDetalle = () => {
@@ -36,7 +37,7 @@ export const HeroeDetalle = () => {
     return (
         <div className="text-center">
             {personaje.map((item) => (
-                <div className='card fade-in bg-black w-96 text-center m-auto mt-10'>
+                <div className='card bg-black w-96 text-center m-auto mt-10 animate__animated animate__bounceInLeft'>
                     <img src={item.imagen} className="object-fill w-96" alt='...' />
                     <h1 className="uppercase font-bold pt-2 text-3xl">{item.nombre}</h1>
                     <p className='p-2 text-xl'>Poderes: {item.descripcion}</p>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import MarvelLogo from '../../src/assets/Marvel.png'
 import DcLogo from '../../src/assets/DC.png'
+import 'animate.css'
 
 export const Home = () => {
   return (
     <div className='text-center'>
    
-    <h1 className=' text-5xl mt-12'>SUPER HEROES DE DC Y MARVEL</h1>
+    <h1 className=' text-5xl mt-12 animate__animated animate__lightSpeedInRight'>SUPER HEROES DE MARVEL Y DC</h1>
     <div className='grid-cols-2 inline-grid text-center gap-6 align-middle mt-20'>
-     <a href="/marvel"><img src={MarvelLogo} className="h-44 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-150 hover:bg-indigo-500 duration-300 rounded-md" alt="" /></a>
-     <a href="/dc"><img src={DcLogo} className="h-44 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-150 hover:bg-indigo-500 duration-300 rounded-md" alt="" /></a>
+     <a href="/marvel"><img src={MarvelLogo} className="h-44 bg-blue-500 rounded-md animate__animated animate__infinite animate__heartBeat animate__slow" alt="" /></a>
+     <a href="/dc"><img src={DcLogo} className="h-44 bg-blue-500  rounded-md animate__animated animate__infinite animate__heartBeat animate__slow" alt="" /></a>
     </div>
     </div>
     
