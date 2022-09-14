956e158f828044de46e3f0053fa2b5da2d501247
xbox360colombia <xbox360colombia@gmail.com>
2022-09-14 14:04:19 -0500

    7242

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter>
+  <HashRouter base="/">
     <App />
-  </BrowserRouter>
+  </HashRouter>
 
 
 )
